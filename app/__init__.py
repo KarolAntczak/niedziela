c6d6707361dedb85a1a6cd90e51a31375dd64da2
Karol <karolczak1>
2019-02-10 19:39:25 +0100

    Added social links

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import os
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_bower import Bower
 from flask_pymongo import PyMongo
 
 locale.setlocale(locale.LC_TIME, "pl")
@@ -10,6 +11,7 @@ locale.setlocale(locale.LC_TIME, "pl")
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI", default=None)
 Bootstrap(app)
+Bower(app)
 mongo = PyMongo(app)
 
 from app import routes
