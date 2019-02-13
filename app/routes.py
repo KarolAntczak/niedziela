f6762f2f37636c88960fb205b6565faa7f207b56
Karol <karolczak1>
2019-02-13 20:01:20 +0100

    Added templates hierarchy. Added 'calendar' template stub

--- app/routes.py
+++ app/routes.py
@@ -12,3 +12,9 @@ def index():
     month = get_month_name(next_sunday)
     shopping = is_shopping_day(next_sunday)
     return render_template('index.html', next_sunday=next_sunday, month=month, shopping=shopping, is_sunday=is_sunday)
+
+
+@app.route('/calendar')
+def calendar():
+    next_sunday = get_next_sunday()
+    return render_template('calendar.html', next_sunday=next_sunday)
